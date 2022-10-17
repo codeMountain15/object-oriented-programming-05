aea33862b12862d2d1f1f2c70faeb87ad3a7de47
Diogenis Alexandrakis <diogenis_Al@yahoo.com>
2022-10-17 21:42:17 +0300

    Create simple_for01.cpp

---
+++ source_code/simple_for01.cpp
@@ -0,0 +1,13 @@
+// simple_for01.cpp
+//
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+    for (int i = 0; i < 10; i++) {
+        cout << i << "\n";
+    } // end of for loop
+        
+}
