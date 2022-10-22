ad22d3a3b9d0738d6cf8e08a819fcb2c874c7a97
Diogenis Alexandrakis <diogenis_Al@yahoo.com>
2022-10-22 14:02:06 +0300

    Update classes_intro01.cpp

--- source_code/classes_intro01.cpp
+++ source_code/classes_intro01.cpp
@@ -11,6 +11,7 @@ public:
     string color;       // white, black etc.
     float weight = 1.0;    // in kilograms
     bool gender;        // male or female
+                        // basically, male sheep are called rams
 
     void speak() { cout << "Mpeeeee\n"; }
     void eat() { weight += 0.5; }
