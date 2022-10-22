8addade4ed9176b6451fd6b4d3f3357dcdf9d8cd
Diogenis Alexandrakis <diogenis_Al@yahoo.com>
2022-10-22 22:09:34 +0300

    Create player.cpp

---
+++ source_code/good_practice/player.cpp
@@ -0,0 +1,34 @@
+// good_practice/player.cpp
+//
+
+#include <cstring>
+#include <iostream>
+#include "player.h"
+
+using namespace std;
+
+// set methods
+void Player::set_username(string str){	
+	username = str;
+}
+
+void Player::set_level(int a1) {
+	level = a1;
+}
+
+void Player::set_id(int a2) {
+	id = a2;
+}
+
+// get methods
+std::string Player::get_username()const {
+	return username;
+}
+
+int Player::get_level()const {
+	return level;
+}
+
+int Player::get_id()const {
+	return id;
+}
