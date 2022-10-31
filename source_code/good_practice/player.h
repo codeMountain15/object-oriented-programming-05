4481c7fec02085b49296fa68452af73ef27449bc
Diogenis Alexandrakis <diogenis_Al@yahoo.com>
2022-10-31 22:06:39 +0200

    Update player.h

--- source_code/good_practice/player.h
+++ source_code/good_practice/player.h
@@ -3,18 +3,20 @@
 
 #pragma once
 
+using namespace std;
+
 class Player {
 private:
-	std::string username;
+	string username;
 	int level;
 	int id;
 
 public:
-	void set_username(std::string); 
+	void set_username(std::string);
 	void set_level(int);
 	void set_id(int);
 
-	std::string get_username()const;
+	string get_username()const;
 	int get_level()const;
 	int get_id()const;
 };
